75cc288adea29f4da2cedb5445a569dab373fdcd
ecstazane <kirkzanemf@gmail.com>
2026-02-12 11:40:28 +0800

    fixed config

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-    testDir: './e2e/tests',
+    testDir: './tests',
     timeout: 30 * 1000,
     expect: {
         timeout: 5000,
     },
     use: {
-        baseURL: 'http://localhost:3000',
+        baseURL: 'http://localhost:5173',
         headless: false,
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
         launchOptions: {
             slowMo: 1000,
         },
-
-
     },
 });
